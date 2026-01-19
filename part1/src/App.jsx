3f5cd56da0482e7bb5e7bdac6ae7fbc1d5e84cac
Matheus Nunes <matheusjobs04@gmail.com>
2026-01-19 20:48:53 -0300

    Handling arrays

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,39 +1,33 @@
 import { useState } from "react"
 
-const Hello = ({name, age}) =>{
-  // const {name, age} = props
-  const bornYear = () => new Date().getFullYear() - age
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} year old
-      </p>
-      <p> So you were probably born in {bornYear()}</p>
-    </div>
-  )
-}
-
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-// contador
-    // setTimeout(
-    //   () => setCounter(counter + 1),
-    //   1000
-    // )
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft (left + 1)
+    setTotal (left + right)
+  }
 
-    const increaseByOne = () => setCounter(counter + 1)
-    const setTozero = () => setCounter(0)
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight (right + 1)
+    setTotal (left + right)
+  }
 
   return(
     <div>
-      {counter}
-
-      <button onClick={increaseByOne}> plus</button>
-
-      <button onClick={setTozero}> zero </button> 
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>left</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+      <p>total {total}</p>
     </div>
   )
-}
 
+}
 export default App

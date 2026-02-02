174d6ffc5f54bb496b4b1bd9f4c815d8b095f26a
Matheus Nunes <matheusjobs04@gmail.com>
2026-02-01 21:02:21 -0300

    rendering colletions

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,20 +1,27 @@
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+import App from './App2'
 
-// let counter = 1
+const notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        important: true
+    },
 
-// const root = ReactDOM.createRoot(document.getElementById9('root'))
+    {
+        id: 2,
+        content: "Browser can execute only Js",
+        important: false
+    },
 
-// const refresh = () => {
-//     root.render(
-//         <App counter={counter}/>
-//     )
-// }
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protoco.",
+        important: true
+    }
+]
 
-// setInterval(() => {
-//     refresh()
-//     counter += 1
-// }, 1000)
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App2 notes={notes}/>)
